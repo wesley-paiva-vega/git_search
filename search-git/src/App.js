3a8d82d6b433f938f4dd60cc1581d7c9c3d90d0a
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2022-05-30 22:26:51 -0300

    mudei o texto pra maycon alypower

--- search-git/src/App.js
+++ search-git/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Maycon presta atençao nas aulas
+          Maycon Alypower
         </p>
         <a
           className="App-link"
