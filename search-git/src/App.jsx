073f74859c7ee557a079025c5e7ae8220313dc27
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2022-05-30 23:34:56 -0300

    primeira estrutura

--- search-git/src/App.jsx
+++ search-git/src/App.jsx
@@ -1,21 +1,67 @@
-import { styled, globalCss } from "@stitches/react";
-import styles from "./styles.css";
+import { styled, globalCss, css } from "@stitches/react";
 
 const Main = globalCss({
   "*": { margin: 0, padding: 0 },
 });
 
-const Content = styled("div", {
+const MainContent = styled("div", {
+  backgroundColor: "#3760DB",
+  height: "100vh",
   display: "flex",
-  justifyContent: "center",
+  flexDirection: "column",
+  alignItems: "center",
+
+  div: {
+    display: "flex",
+    gap: "20px",
+    padding: "20px",
+  },
+
+  h1: {
+    marginTop: "20px",
+    color: "#E8A92E",
+  },
+});
+
+const ButtonDoMal = css({
+  width: "110px",
+  padding: "10px 10px",
+  borderRadius: "10px",
+  border: "none",
+  fontSize: "18px",
+  fontWeight: "bold",
+  color: "white",
+  fontFamily: "system-ui",
+  backgroundColor: "#030f3478",
+
+  "&:hover": {
+    filter: "brightness(0.6);",
+    cursor: "pointer",
+  },
+});
+
+const InputDoMal = styled("input", {
+  width: "350px",
+  outline: "none",
+  fontSize: "16px",
+  fontWeight: "bold",
+  color: "#2c7bd0",
+  fontFamily: "system-ui",
 });
 
 function App() {
   return (
     <div className={Main()}>
-      <div>
-        <h1>GitHub Search</h1>
-      </div>
+      <MainContent>
+        <h1>GitHub Searchh</h1>
+        <div>
+          <InputDoMal
+            placeholder="Digite um nick do github para pesquizar"
+            type="text"
+          />
+          <button className={ButtonDoMal()}>Pesquisar github</button>
+        </div>
+      </MainContent>
     </div>
   );
 }
